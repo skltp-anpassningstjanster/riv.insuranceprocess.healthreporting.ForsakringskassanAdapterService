babdfae75f1abca7059174a713d47f6bdaba66ff
mats.ekhammar@callistaenterprise.se <mats.ekhammar@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2013-09-03 09:02:57 +0000

    Added 2 new teststubs for SendMedicalCertificate and RevokeMedicalCertificate with configuration changes.

---
+++ FkEintygAdapterIC/src/test/java/se/skl/skltpservices/adapter/fk/revokemedcert/RevokeMedCertImpl.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2013 Center for eHalsa i samverkan (CeHis).
+ * 							<http://cehis.se/>
+ *
+ * This file is part of SKLTP.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package se.skl.skltpservices.adapter.fk.revokemedcert;
+
+import javax.jws.WebService;
+
+import org.w3.wsaddressing10.AttributedURIType;
+
+import se.skl.riv.insuranceprocess.healthreporting.revokemedicalcertificate.v1.rivtabp20.RevokeMedicalCertificateResponderInterface;
+import se.skl.riv.insuranceprocess.healthreporting.revokemedicalcertificateresponder.v1.RevokeMedicalCertificateRequestType;
+import se.skl.riv.insuranceprocess.healthreporting.revokemedicalcertificateresponder.v1.RevokeMedicalCertificateResponseType;
+import se.skl.riv.insuranceprocess.healthreporting.v2.ErrorIdEnum;
+import se.skl.riv.insuranceprocess.healthreporting.v2.ResultCodeEnum;
+import se.skl.riv.insuranceprocess.healthreporting.v2.ResultOfCall;
+
+
+@WebService(
+		serviceName = "RevokeMedicalCertificateResponderService", 
+		endpointInterface="se.skl.riv.insuranceprocess.healthreporting.revokemedicalcertificate.v1.rivtabp20.RevokeMedicalCertificateResponderInterface", 
+		portName = "RevokeMedicalCertificateResponderPort", 
+		targetNamespace = "urn:riv:insuranceprocess:healthreporting:RevokeMedicalCertificate:1:rivtabp20",
+		wsdlLocation = "schemas/vard/interactions/RevokeMedicalCertificateInteraction/RevokeMedicalCertificateInteraction_1.0_rivtabp20.wsdl")
+public class RevokeMedCertImpl implements RevokeMedicalCertificateResponderInterface {
+
+	public RevokeMedicalCertificateResponseType revokeMedicalCertificate(
+			AttributedURIType logicalAddress,
+			RevokeMedicalCertificateRequestType parameters) {
+		try {
+			RevokeMedicalCertificateResponseType response = new RevokeMedicalCertificateResponseType();
+			String logiskAdress = logicalAddress.getValue();
+			String name = "dummy";
+			// Check if we should throw some kind of exception or simulate a timeout.
+			if (    parameters != null && parameters.getRevoke().getLakarutlatande() != null && 
+					parameters.getRevoke().getLakarutlatande().getPatient() != null && 
+					parameters.getRevoke().getLakarutlatande().getPatient().getFullstandigtNamn() != null &&
+					parameters.getRevoke().getLakarutlatande().getPatient().getFullstandigtNamn().length() > 0) {
+
+				name = parameters.getRevoke().getLakarutlatande().getPatient().getFullstandigtNamn();
+			}
+				
+			if (name.equalsIgnoreCase("Error") || logiskAdress.contains("Error") ) {
+				ResultOfCall result = new ResultOfCall();
+				result.setResultCode(ResultCodeEnum.ERROR);
+				result.setErrorId(ErrorIdEnum.APPLICATION_ERROR);
+				response.setResult(result);
+				System.out.println("Returned Error for not validating!");
+			} else if (name.equalsIgnoreCase("NoCertificate") || logiskAdress.contains("NoCertificate")) {
+				ResultOfCall result = new ResultOfCall();
+				result.setResultCode(ResultCodeEnum.ERROR);
+				result.setErrorId(ErrorIdEnum.APPLICATION_ERROR);
+				result.setErrorText("No Medical Certificate found that could be sent");
+				response.setResult(result);
+				System.out.println("Returned Error for medical certificate not found!");
+			} else if (name.equalsIgnoreCase("AlreadyRevoked") || logiskAdress.contains("AlreadyRevoked")) {
+				ResultOfCall result = new ResultOfCall();
+				result.setResultCode(ResultCodeEnum.INFO);
+				result.setInfoText("Medical Certificate already revoke");
+				response.setResult(result);
+				System.out.println("Returned Info for already revoked certificate!");
+			} else if (name.equalsIgnoreCase("Exception") || logiskAdress.contains("Exception")) {
+				System.out.println("Returned Exception for not validating!");
+				throw new RuntimeException("Exception called");
+			} else if (name.equalsIgnoreCase("Timeout") || logiskAdress.contains("Timeout")) {
+				System.out.println("Returned Timeout for not validating!");
+				Thread.currentThread();
+				try {
+					Thread.sleep(60000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			} else {
+				ResultOfCall resCall = new ResultOfCall();
+				resCall.setResultCode(ResultCodeEnum.OK);
+				response.setResult(resCall);				
+				System.out.println("Returned OK for not validating!");
+			}
+
+			return response;
+		} catch (RuntimeException e) {
+			System.out.println("Error occured: " + e);
+			throw e;
+		}
+	}
+}
---
+++ FkEintygAdapterIC/src/test/java/se/skl/skltpservices/adapter/fk/revokemedcert/RevokeMedCertValidateImpl.java
@@ -0,0 +1,266 @@
+/**
+ * Copyright (c) 2013 Center for eHalsa i samverkan (CeHis).
+ * 							<http://cehis.se/>
+ *
+ * This file is part of SKLTP.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package se.skl.skltpservices.adapter.fk.revokemedcert;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.jws.WebService;
+
+import org.w3.wsaddressing10.AttributedURIType;
+
+import se.skl.riv.insuranceprocess.healthreporting.qa.v1.Amnetyp;
+import se.skl.riv.insuranceprocess.healthreporting.qa.v1.LakarutlatandeEnkelType;
+import se.skl.riv.insuranceprocess.healthreporting.revokemedicalcertificate.v1.rivtabp20.RevokeMedicalCertificateResponderInterface;
+import se.skl.riv.insuranceprocess.healthreporting.revokemedicalcertificateresponder.v1.RevokeMedicalCertificateRequestType;
+import se.skl.riv.insuranceprocess.healthreporting.revokemedicalcertificateresponder.v1.RevokeMedicalCertificateResponseType;
+import se.skl.riv.insuranceprocess.healthreporting.revokemedicalcertificateresponder.v1.RevokeType;
+import se.skl.riv.insuranceprocess.healthreporting.v2.EnhetType;
+import se.skl.riv.insuranceprocess.healthreporting.v2.ErrorIdEnum;
+import se.skl.riv.insuranceprocess.healthreporting.v2.HosPersonalType;
+import se.skl.riv.insuranceprocess.healthreporting.v2.PatientType;
+import se.skl.riv.insuranceprocess.healthreporting.v2.ResultCodeEnum;
+import se.skl.riv.insuranceprocess.healthreporting.v2.ResultOfCall;
+import se.skl.riv.insuranceprocess.healthreporting.v2.VardgivareType;
+
+/**
+ * Validation class that will certify a webservice call made for an answer regarding a medical certificate.. We will check mandatory/optional fields and all other declared rules.
+ * @author matsek
+ *
+ */
+
+@WebService(
+		serviceName = "RevokeMedicalCertificateResponderService", 
+		endpointInterface="se.skl.riv.insuranceprocess.healthreporting.revokemedicalcertificate.v1.rivtabp20.RevokeMedicalCertificateResponderInterface", 
+		portName = "RevokeMedicalCertificateResponderPort", 
+		targetNamespace = "urn:riv:insuranceprocess:healthreporting:RevokeMedicalCertificate:1:rivtabp20",
+		wsdlLocation = "schemas/vard/interactions/RevokeMedicalCertificateInteraction/RevokeMedicalCertificateInteraction_1.0_rivtabp20.wsdl")
+public class RevokeMedCertValidateImpl implements RevokeMedicalCertificateResponderInterface {
+
+	public RevokeMedicalCertificateResponseType revokeMedicalCertificate(
+			AttributedURIType logicalAddress,
+			RevokeMedicalCertificateRequestType parameters) {
+
+		// List of validation errors
+		ArrayList<String> validationErrors = new ArrayList<String>();
+
+		// Create a response and set result of validation            
+		RevokeMedicalCertificateResponseType outResponse = new RevokeMedicalCertificateResponseType();
+		ResultOfCall outResCall = new ResultOfCall();
+		outResponse.setResult(outResCall);
+
+		// Validate incoming request
+		try {
+			// Check that we got any data at all
+			if (parameters == null) {
+				validationErrors.add("No RevokeMedicalCertificate found in incoming data!");
+				throw new Exception();
+			}
+			
+			// Check that we got an answer element
+			if (parameters.getRevoke() == null) {
+				validationErrors.add("No Revoke element found in incoming request data!");
+				throw new Exception();
+			}
+			
+			RevokeType inRevoke = parameters.getRevoke();
+			
+			/**
+			 *  Check meddelande data + lakarutlatande reference
+			 */
+			
+			// Meddelande id vården - mandatory
+			if ( inRevoke.getVardReferensId() == null ||
+					inRevoke.getVardReferensId().length() < 1 ) {
+				 validationErrors.add("No vardReferens-id found!");				
+			}
+			
+			// Avsänt tidpunkt - mandatory
+            if (inRevoke.getAvsantTidpunkt() == null || !inRevoke.getAvsantTidpunkt().isValid()) {
+				validationErrors.add("No or wrong avsantTidpunkt found!");				
+            }
+						
+			// Läkarutlåtande referens - mandatory
+            if (inRevoke.getLakarutlatande() == null ) {
+				validationErrors.add("No lakarutlatande element found!");	
+				throw new Exception();
+            }
+            LakarutlatandeEnkelType inLakarUtlatande = inRevoke.getLakarutlatande();
+            
+			// Läkarutlåtande referens - id - mandatory
+			if ( inLakarUtlatande.getLakarutlatandeId() == null ||
+				inLakarUtlatande.getLakarutlatandeId().length() < 1 ) {
+				validationErrors.add("No lakarutlatande-id found!");				
+			}
+
+			// Läkarutlåtande referens - signeringsTidpunkt - mandatory
+            if (inLakarUtlatande.getSigneringsTidpunkt() == null || !inLakarUtlatande.getSigneringsTidpunkt().isValid()) {
+				validationErrors.add("No or wrong lakarutlatande-signeringsTidpunkt found!");				
+            }
+
+			// Läkarutlåtande referens - patient - mandatory
+            if (inLakarUtlatande.getPatient() == null ) {
+				validationErrors.add("No lakarutlatande patient element found!");	
+				throw new Exception();
+            }
+            PatientType inPatient = inLakarUtlatande.getPatient();
+            
+			// Läkarutlåtande referens - patient - personid mandatory
+            // Check patient id - mandatory
+			if (inPatient.getPersonId() == null ||	
+				inPatient.getPersonId().getExtension() == null ||	
+				inPatient.getPersonId().getExtension().length() < 1) {
+				validationErrors.add("No lakarutlatande-Patient Id found!");								
+			}
+			
+			// Check patient o.i.d.
+			if (inPatient.getPersonId() == null ||	
+				inPatient.getPersonId().getRoot() == null ||	
+				(!inPatient.getPersonId().getRoot().equalsIgnoreCase("1.2.752.129.2.1.3.1") && !inPatient.getPersonId().getRoot().equalsIgnoreCase("1.2.752.129.2.1.3.3"))) {
+					validationErrors.add("Wrong o.i.d. for Patient Id! Should be 1.2.752.129.2.1.3.1 or 1.2.752.129.2.1.3.3");								
+				}
+			String inPersonnummer = inPatient.getPersonId().getExtension();
+
+	        // Check format of patient id - personnummer valid format is 19121212-1212 or 19121212+1212
+//			if (!Pattern.matches("[0-9]{8}[-+][0-9]{4}", inPersonnummer) ) {
+//				validationErrors.add("Wrong format for person-id! Valid format is YYYYMMDD-XXXX or YYYYMMDD+XXXX.");												
+//			}
+            
+			// Läkarutlåtande referens - patient - namn - mandatory
+			if (inPatient.getFullstandigtNamn() == null || inPatient.getFullstandigtNamn().length() < 1 ) {
+				validationErrors.add("No lakarutlatande Patient fullstandigtNamn elements found or set!");								
+			}
+								
+			/**
+			 *  Check avsändar data.
+			 */
+			if (inRevoke.getAdressVard() == null) {
+				validationErrors.add("No adressVard element found!");				
+				throw new Exception();
+			}
+			if ( inRevoke.getAdressVard().getHosPersonal() == null) {
+				validationErrors.add("No adressVard - hosPersonal element found!");				
+				throw new Exception();
+			}	
+			HosPersonalType inHoSP = inRevoke.getAdressVard().getHosPersonal();
+			
+		    // Check lakar id - mandatory
+	        if (inHoSP.getPersonalId() == null || 
+	        	inHoSP.getPersonalId().getExtension() == null ||
+	        	inHoSP.getPersonalId().getExtension().length() < 1) {
+				validationErrors.add("No personal-id found!");	            	
+	        }
+	        // Check lakar id o.i.d.
+	        if (inHoSP.getPersonalId() == null || 
+	        	inHoSP.getPersonalId().getRoot() == null ||
+	            !inHoSP.getPersonalId().getRoot().equalsIgnoreCase("1.2.752.129.2.1.4.1")) {
+				validationErrors.add("Wrong o.i.d. for personalId! Should be 1.2.752.129.2.1.4.1");								
+	        }
+	        
+	        // Check lakarnamn - mandatory
+			if (inHoSP.getFullstandigtNamn() == null || inHoSP.getFullstandigtNamn().length() < 1 ) {
+				validationErrors.add("No skapadAvHosPersonal fullstandigtNamn elements found or set!");								
+			}
+
+	        // Check that we got a enhet element
+	        if (inHoSP.getEnhet() == null) {
+				validationErrors.add("No enhet element found!");	  
+				throw new Exception();
+	        }
+	        EnhetType inEnhet = inHoSP.getEnhet() ;
+	       
+	        // Check enhets id - mandatory
+	        if (inEnhet.getEnhetsId() == null ||
+	        	inEnhet.getEnhetsId().getExtension() == null ||
+	        	inEnhet.getEnhetsId().getExtension().length() < 1) {
+				validationErrors.add("No enhets-id found!");	            	
+	        }
+	        // Check enhets o.i.d
+	        if (inEnhet.getEnhetsId() == null || 
+	        	inEnhet.getEnhetsId().getRoot() == null ||
+	            !inEnhet.getEnhetsId().getRoot().equalsIgnoreCase("1.2.752.129.2.1.4.1")) {
+				validationErrors.add("Wrong o.i.d. for enhetsId! Should be 1.2.752.129.2.1.4.1");								
+	        }
+	        
+	        // Check enhetsnamn - mandatory
+	        if (inEnhet.getEnhetsnamn() == null || 
+	        	inEnhet.getEnhetsnamn().length() < 1) {
+	        	validationErrors.add("No enhetsnamn found!");	            	
+	        }
+
+	        // Check that we got a vardgivare element
+	        if (inEnhet.getVardgivare() == null) {
+				validationErrors.add("No vardgivare element found!");	  
+				throw new Exception();
+	        }
+	        VardgivareType inVardgivare = inEnhet.getVardgivare();
+	       
+	        // Check vardgivare id - mandatory
+	        if (inVardgivare.getVardgivareId() == null ||
+	        	inVardgivare.getVardgivareId().getExtension() == null ||
+	        	inVardgivare.getVardgivareId().getExtension().length() < 1) {
+				validationErrors.add("No vardgivare-id found!");	            	
+	        }
+	        // Check vardgivare o.i.d.
+	        if (inVardgivare.getVardgivareId() == null || 
+	        	inVardgivare.getVardgivareId().getRoot() == null ||
+	            !inVardgivare.getVardgivareId().getRoot().equalsIgnoreCase("1.2.752.129.2.1.4.1")) {
+	        	validationErrors.add("Wrong o.i.d. for vardgivareId! Should be 1.2.752.129.2.1.4.1");								
+	        }
+
+	        // Check vardgivarename - mandatory
+	        if (inVardgivare.getVardgivarnamn() == null || 
+	        	inVardgivare.getVardgivarnamn().length() < 1) {
+				validationErrors.add("No vardgivarenamn found!");	            	
+	        }
+			
+			// Check if we got any validation errors that not caused an Exception
+			if (validationErrors.size() > 0) {
+				throw new Exception();
+			} 
+			
+			// No validation errors! Return OK!            
+			outResCall.setResultCode(ResultCodeEnum.OK);
+			outResponse.setResult(outResCall);
+
+			return outResponse;
+		} catch (Exception e) {
+			outResCall.setErrorText(getValidationErrors(validationErrors));
+			outResCall.setErrorId(ErrorIdEnum.VALIDATION_ERROR);
+			outResCall.setResultCode(ResultCodeEnum.ERROR);
+			return outResponse;
+		}
+	}
+	
+	private String getValidationErrors(ArrayList<String> validationErrors) {
+		int i = 1;
+		StringBuffer validationString = new StringBuffer();
+		Iterator<String> iterValidationErrors = validationErrors.iterator();
+		validationString.append("Validation error " + i++ + ":");
+		validationString.append((String)iterValidationErrors.next());
+		while (iterValidationErrors.hasNext()) {
+			validationString.append("\n\rValidation error " + i++ + ":");
+			validationString.append((String)iterValidationErrors.next());
+		}
+		return validationString.toString();
+	}
+	
+}
